af1db8ba64c06c08c2ae06e3f77c06e5a20e6c8f
EndrII <EndrIIMail@gmail.com>
2024-01-14 13:54:56 +0100

    added first implementation

--- src/example/languages/ru.ts
+++ src/example/languages/ru.ts
@@ -15,5 +15,61 @@
         <source>Thank you for your wait.</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Service is stoped. Please use the start command.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You are try to run multiple commands. please choose some of pleas choose one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>please choose one of available actions add/remove/get</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You forget a data. please use the next command add -data yourDataString</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You forget a hash key of alias of removable data. Please use the next command remove -hash yourHash or remove -alias yourAlias</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You forget a hash key of alias of getting data. Please use the next command get -hash yourHash or get -alias yourAlias</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This is description of the ping command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Returns state</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Adds new value into seret storage, and return a hash key after adding.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove data from storage by hash key or alias.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>return secret from database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alias name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data that will be saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hash kay of data</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
